708adf514a683eb5de5166e302d44285dce802ea
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-02 11:00:00 +0700

    implementasi fungsi perkenalan di derivedClass

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class basedClass{
-    public :
-    virtual void perkenalan(){
+class basedClass final
+{
+public:
+    virtual void perkenalan()
+    {
         cout << "Hallo saya Function dari base class";
     }
 };
+class derivedClass : public basedClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hallo saya Funtion dari derived Class";
+    }
+};
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+    return 0;
+}
