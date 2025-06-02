956e4bc7761b7c7ac90c4badde8d121ab2ead29b
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-02 10:17:27 +0700

    membuat main function

--- Enkapsulasi/enkapsulasi.cpp
+++ Enkapsulasi/enkapsulasi.cpp
@@ -15,3 +15,19 @@ public:
         return saklarNo[i];
     }
 };
+
+int main(){
+    remotLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+
+    cout << lampuRumah.getSaklarNo(0)<<endl;
+    cout << lampuRumah.getSaklarNo(1)<<endl;
+    cout << lampuRumah.getSaklarNo(2)<<endl;
+    cout << lampuRumah.getSaklarNo(3)<<endl;
+    return 0;
+}
