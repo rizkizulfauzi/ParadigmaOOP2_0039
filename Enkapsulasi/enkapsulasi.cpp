5d6d184a4ab63480e0334ac1ccf76597ad27a877
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-02 10:11:09 +0700

    implementasi akses saklarNo

--- Enkapsulasi/enkapsulasi.cpp
+++ Enkapsulasi/enkapsulasi.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
-class remotLampu{
+class remotLampu
+{
 private:
-string saklarNo[10];
+    string saklarNo[10];
 
-
+public:
+    void setSaklarNo(int i, string value)
+    {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i){
+        return saklarNo[i];
+    }
 };
