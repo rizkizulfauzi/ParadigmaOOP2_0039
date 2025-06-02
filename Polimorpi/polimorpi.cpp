a0bba10a8715d130b5c1ad376e0ba97dea028e87
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-02 11:15:47 +0700

    membuat main function

--- Polimorpi/polimorpi.cpp
+++ Polimorpi/polimorpi.cpp
@@ -3,19 +3,30 @@ using namespace std;
 class seseorang
 {
 public:
-virtual void pesab() = 0;
+virtual void pesan() = 0;
 };
 
 class joko : public seseorang {
     public:
-    void pesan(){
+    void pesan() override {
         cout << "Pesan dari joko" << endl;
     }
 };
 
 class lia : public seseorang {
     public :
-    void pesan (){
+    void pesan() override {
         cout << "Pesan dari lia" << endl;
     }
 };
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    return 0;
+}
